a4f1008627a38b8551fa2b73241b6744a35f4e78
iionly <iionly@gmx.de>
2013-11-17 19:42:44 +0100

    Initial release for Elgg 1.9; see README.txt

---
+++ userpoints_balance/languages/de.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Gain userpoints for hours worked - a skeletal plugin that might need to get customized according to your needs.
+ *
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author iionly
+ * @copyright iionly
+ */
+
+return array(
+    'userpoints_balance:menu' => 'Aktivitätspunkte-Gutschrift',
+    'userpoints_balance:title' => 'Aktivitätspunkte-Gutschrift',
+
+    'userpoints_balance:overview' => 'Auf dieser Seite kannst Du berichten, wie viele Stunden Du an Revisionen gearbeitet hast. Für jede gearbeitete Stunde bekommst Du %s Aktivitätspunkte als Belohnung.',
+    'userpoints_balance:hours_worked' => 'Wie viele Stunden hast Du an Revisionen gearbeitet? ',
+    'userpoints_balance:description' => 'Wenn Du willst (Eingabe optional), kannst Du kurz beschreiben, woran Du gearbeitet hast: ',
+    'userpoints_balance:proceed' => 'Belohne mich!',
+
+    'userpoints_balance:no_description' => '%s hat keinen Begleittext eingegeben.',
+
+    'userpoints_balance:pointfail' => "Bei der Verwendung der Userpoints-API trat ein Fehler auf!",
+    'userpoints_balance:pointsuccess' => "Dir wurden %s Aktivitätspunkte für %s Stunden Arbeit an Revisionen gutgeschrieben."
+);
